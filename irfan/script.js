3f341e4ca86ad97a5d92fb5448af4e408a13e084
SaddadSium <saddadsium.official@gmail.com>
2025-12-03 23:51:16 +0600

    ADD JAVASCRIPT TASK 8

---
+++ irfan/script.js
@@ -0,0 +1,57 @@
+function handleUserType() {
+  let userType = document.getElementById("userType").value;
+  let rollDiv = document.getElementById("rollDiv");
+  let deptDiv = document.getElementById("deptDiv");
+  let message = document.getElementById("message");
+
+  message.innerText = "";
+  dataDisplay.innerText = "";
+
+  if (userType === "student") {
+    rollDiv.style.display = "block";
+    deptDiv.style.display = "none";
+  } else if (userType === "teacher") {
+    deptDiv.style.display = "block";
+    rollDiv.style.display = "none";
+  } else {
+    rollDiv.style.display = "none";
+    deptDiv.style.display = "none";
+  }
+}
+
+function handleSubmit() {
+  let userType = document.getElementById("userType").value;
+  let roll = document.getElementById("rollInput").value.trim();
+  let dept = document.getElementById("deptInput").value.trim();
+
+  let message = document.getElementById("message");
+  let dataDisplay = document.getElementById("dataDisplay");
+
+  message.innerText = "";
+  dataDisplay.innerText = "";
+
+  if (userType === "student") {
+    if (roll === "") {
+      message.style.color = "red";
+      message.innerText = "Please enter your roll number!";
+      return;
+    }
+
+    message.style.color = "#27ae60";
+    message.innerText = "Student roll submitted";
+    dataDisplay.innerText = `Roll: ${roll}`;
+  } else if (userType === "teacher") {
+    if (dept === "") {
+      message.style.color = "red";
+      message.innerText = "Please enter your department!";
+      return;
+    }
+
+    message.style.color = "#2980b9";
+    message.innerText = "Teacher department submitted";
+    dataDisplay.innerText = `Department: ${dept}`;
+  } else {
+    message.style.color = "red";
+    message.innerText = "Please select a user type first!";
+  }
+}
